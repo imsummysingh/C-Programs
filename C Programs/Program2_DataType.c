6646e4cda595107701a9e2927a59d5fc262eb45f
Summy Singh <summysingh86@gmail.com>
2020-11-09 02:09:24 +0530

    prog added

---
+++ C Programs/Program2_DataType.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+int main() {
+    
+    int x;
+    float y;
+    double d;
+    
+    x=4;
+    y=2.0;
+    d=3.1409876;
+    
+    printf("The value of x is: %d\n",x);
+    printf("The value of x is: %f\n",y);
+    printf("The value of x is: %lf",d);
+    
+    return 0;
+}
